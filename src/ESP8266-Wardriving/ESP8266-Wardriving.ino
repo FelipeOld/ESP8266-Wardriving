48e80128fd79135a843a19b05f6d532b61ec01a9
Felipe Baptista <felipesouzabaptista@gmail.com>
2023-10-14 23:41:44 -0300

    added network check before store in sd card
    not the best implementation, slows interation but works.

--- src/ESP8266-Wardriving/ESP8266-Wardriving.ino
+++ src/ESP8266-Wardriving/ESP8266-Wardriving.ino
@@ -123,78 +123,80 @@ void lookForNetworks() {
   else {
     for (int i = 0; i < n; ++i) {
       if ((WiFi.channel(i) > 0) && (WiFi.channel(i) < 15)) {
-        display.clearDisplay();
-        display.setCursor(48, 0);
-        sprintf_P(currentTime, PSTR("%02d:%02d"),hour(),minute());
-        display.println(currentTime);
-        //print line do simplify data view in serial monitor
-        Serial.println("------------- NEW DATA INCOMING! -------------");
-        //print date and time on serial monitor
-        Serial.println("Hora: " + String(currentTime));    
-        display.drawLine(0,3,46,3,WHITE);
-        display.drawLine(79,3,128,3,WHITE);
-        display.display();    
-        networks++;
-        File logFile = SD.open(logFileName, FILE_WRITE);
-        logFile.print(WiFi.BSSIDstr(i));  logFile.print(',');
-        logFile.print(WiFi.SSID(i)); logFile.print(',');
-        display.setCursor(0,10);
-        if (WiFi.SSID(i).length() > 20) { 
-          display.println(WiFi.SSID(i).substring(0, 18 ) + "...");
-          //if network name is above 20 chars, print in serial monitor with the next line
-          Serial.println(WiFi.SSID(i).substring(0, 18 ) + "...");
-        }
-        else { 
-          display.println(WiFi.SSID(i));
-          //if network name is below 20 chars, print with next line
-          Serial.println("Rede: " + WiFi.SSID(i));
-        }
-        String bssid = WiFi.BSSIDstr(i);
-        bssid.replace(":", "");
-        display.println(bssid + "    (" + WiFi.RSSI(i) + ")");
-        //print MAC address from AP e power of signal on serial monitor
-        Serial.println("MAC AP: "+ bssid + "    Potência: (" + WiFi.RSSI(i) + ")");
-        logFile.print(getEncryption(i,"")); logFile.print(',');
-        display.print("Enc: "+getEncryption(i,"screen"));
-        //print encryption used in serial monitor
-        Serial.print("Enc: "+getEncryption(i,"screen"));
-        display.println("   Ch: "+ String(WiFi.channel(i)));
-        //print wireless channel on serial monitor
-        Serial.println("   Canal: "+ String(WiFi.channel(i)));    
-        display.println();    
-        display.setCursor(0,40);
-        logFile.print(year());   logFile.print('-');
-        logFile.print(month());  logFile.print('-');
-        logFile.print(day());    logFile.print(' ');
-        logFile.print(hour());   logFile.print(':');
-        logFile.print(minute()); logFile.print(':');
-        logFile.print(second()); logFile.print(',');
-        logFile.print(WiFi.channel(i)); logFile.print(',');
-        logFile.print(WiFi.RSSI(i)); logFile.print(',');
-        logFile.print(tinyGPS.location.lat(), 6); logFile.print(',');
-        logFile.print(tinyGPS.location.lng(), 6); logFile.print(',');
-        display.println("Networks: " + String(networks));
-        //print value of code loops using the the "networks" var in serial monitor
-        Serial.println("Networks: " + String(networks));
-        display.print(String(int(tinyGPS.speed.mph())) + " MPH");
-        //print speed in serial monitor (??? useless, I know, but if you want to validate something...)
-        Serial.print(String(int(tinyGPS.speed.mph())) + " MPH");
-        display.println(" Sats: " + String(tinyGPS.satellites.value()));
-        //print satellites in serial monitor
-        Serial.println(" Sats: " + String(tinyGPS.satellites.value()));
-        display.println("(" + String(tinyGPS.location.lat(), 5) + "," + String(tinyGPS.location.lng(), 5) + ")");
-        //print coordinates in serial monitor
-        Serial.println("Coordenadas: (" + String(tinyGPS.location.lat(), 5) + "," + String(tinyGPS.location.lng(), 5) + ")");
-        logFile.print(tinyGPS.altitude.meters(), 1); logFile.print(',');
-        logFile.print(tinyGPS.hdop.value(), 1); logFile.print(',');
-        logFile.println("WIFI");
-        logFile.close();
-        display.display();
-        if (getEncryption(i,"")=="[WEP][ESS]"){  // flash if WEP detected
-          display.invertDisplay(true);  delay(200);
-          display.invertDisplay(false); delay(200);
-          display.invertDisplay(true);  delay(200);
-          display.invertDisplay(false); delay(200);
+        if (!isOnFile(WiFi.BSSIDstr(i))) {
+          display.clearDisplay();
+          display.setCursor(48, 0);
+          sprintf_P(currentTime, PSTR("%02d:%02d"),hour(),minute());
+          display.println(currentTime);
+          //print line to simplify data view in serial monitor
+          Serial.println("------------- NEW DATA INCOMING! -------------");
+          //print date and time on serial monitor
+          Serial.println("Hora: " + String(currentTime));    
+          display.drawLine(0,3,46,3,WHITE);
+          display.drawLine(79,3,128,3,WHITE);
+          display.display();    
+          networks++;
+          File logFile = SD.open(logFileName, FILE_WRITE);
+          logFile.print(WiFi.BSSIDstr(i));  logFile.print(',');
+          logFile.print(WiFi.SSID(i)); logFile.print(',');
+          display.setCursor(0,10);
+          if (WiFi.SSID(i).length() > 20) { 
+            display.println(WiFi.SSID(i).substring(0, 18 ) + "...");
+            //if network name is above 20 chars, print in serial monitor with the next line
+            Serial.println(WiFi.SSID(i).substring(0, 18 ) + "...");
+          }
+          else { 
+            display.println(WiFi.SSID(i));
+            //if network name is below 20 chars, print with next line
+            Serial.println("Rede: " + WiFi.SSID(i));
+          }
+          String bssid = WiFi.BSSIDstr(i);
+          bssid.replace(":", "");
+          display.println(bssid + "    (" + WiFi.RSSI(i) + ")");
+          //print MAC address from AP e power of signal on serial monitor
+          Serial.println("MAC AP: "+ bssid + "    Potência: (" + WiFi.RSSI(i) + ")");
+          logFile.print(getEncryption(i,"")); logFile.print(',');
+          display.print("Enc: "+getEncryption(i,"screen"));
+          //print encryption used in serial monitor
+          Serial.print("Enc: "+getEncryption(i,"screen"));
+          display.println("   Ch: "+ String(WiFi.channel(i)));
+          //print wireless channel on serial monitor
+          Serial.println("   Canal: "+ String(WiFi.channel(i)));    
+          display.println();    
+          display.setCursor(0,40);
+          logFile.print(year());   logFile.print('-');
+          logFile.print(month());  logFile.print('-');
+          logFile.print(day());    logFile.print(' ');
+          logFile.print(hour());   logFile.print(':');
+          logFile.print(minute()); logFile.print(':');
+          logFile.print(second()); logFile.print(',');
+          logFile.print(WiFi.channel(i)); logFile.print(',');
+          logFile.print(WiFi.RSSI(i)); logFile.print(',');
+          logFile.print(tinyGPS.location.lat(), 6); logFile.print(',');
+          logFile.print(tinyGPS.location.lng(), 6); logFile.print(',');
+          display.println("Networks: " + String(networks));
+          //print value of code loops using the the "networks" var in serial monitor
+          Serial.println("Networks: " + String(networks));
+          display.print(String(int(tinyGPS.speed.mph())) + " MPH");
+          //print speed in serial monitor (??? useless, I know, but if you want to validate something...)
+          Serial.print(String(int(tinyGPS.speed.mph())) + " MPH");
+          display.println(" Sats: " + String(tinyGPS.satellites.value()));
+          //print satellites in serial monitor
+          Serial.println(" Sats: " + String(tinyGPS.satellites.value()));
+          display.println("(" + String(tinyGPS.location.lat(), 5) + "," + String(tinyGPS.location.lng(), 5) + ")");
+          //print coordinates in serial monitor
+          Serial.println("Coordenadas: (" + String(tinyGPS.location.lat(), 5) + "," + String(tinyGPS.location.lng(), 5) + ")");
+          logFile.print(tinyGPS.altitude.meters(), 1); logFile.print(',');
+          logFile.print(tinyGPS.hdop.value(), 1); logFile.print(',');
+          logFile.println("WIFI");
+          logFile.close();
+          display.display();
+          if (getEncryption(i,"")=="[WEP][ESS]"){  // flash if WEP detected
+            display.invertDisplay(true);  delay(200);
+            display.invertDisplay(false); delay(200);
+            display.invertDisplay(true);  delay(200);
+            display.invertDisplay(false); delay(200);
+          }
         }
       }
     }
@@ -222,22 +224,20 @@ static void smartDelay(unsigned long ms) {
   } while (millis() - start < ms);
 }
 
-/*int isOnFile(String mac) {
-  File netFile = SD.open(logFileName);
-  String currentNetwork;
-  if (netFile) {
-    while (netFile.available()) {
-      currentNetwork = netFile.readStringUntil('\n');
-      if (currentNetwork.indexOf(mac) != -1) {
-        netFile.close();
-        return currentNetwork.indexOf(mac);
+bool isOnFile(String bssid) {
+  File logFile = SD.open(logFileName, FILE_READ);
+  if (logFile) {
+    while (logFile.available()) {
+      String line = logFile.readStringUntil('\n');
+      if (line.indexOf(bssid) != -1) {
+        logFile.close();
+        return true; // O BSSID já está no arquivo
       }
     }
-    netFile.close();
-    return currentNetwork.indexOf(mac);
+    logFile.close();
   }
+  return false; // O BSSID ainda não foi registrado no arquivo
 }
-*/
 
 void initializeSD() { // create new CSV file and add WiGLE headers
   int i = 0; logFileName = "log0.csv";
